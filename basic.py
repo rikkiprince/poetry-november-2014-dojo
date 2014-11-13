a5c20e11c874632cde6677f35ceb422d74ad4ecb
Rikki Prince <rikki@prince.io>
2014-11-13 19:57:51 +0000

    First approach to randomly generate haikus.

---
+++ basic.py
@@ -0,0 +1,16 @@
+import random
+
+five = ["I really like toast", 
+        "I like it best cold", 
+        "Beans are kind to hearts", 
+        "And then do big farts", 
+        "The sky is so blue", 
+        "I love the summer", ]
+
+seven = [   "The sun is so warm up high", 
+            "It is yummy when it's hot", 
+            "I like to eat them daily"]
+
+print random.choice(five)
+print random.choice(seven)
+print random.choice(five)
