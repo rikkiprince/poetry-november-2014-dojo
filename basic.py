e59ff06b15c16c393f62b0fb30ce6954092dece6
Rikki Prince <rikki@prince.io>
2014-11-13 20:15:32 +0000

    Moved data out to separate file.

--- basic.py
+++ basic.py
@@ -1,15 +1,6 @@
 import random
-
-five = ["I really like toast", 
-        "I like it best cold", 
-        "Beans are kind to hearts", 
-        "And then do big farts", 
-        "The sky is so blue", 
-        "I love the summer", ]
-
-seven = [   "The sun is so warm up high", 
-            "It is yummy when it's hot", 
-            "I like to eat them daily"]
+from five import five
+from seven import seven
 
 print random.choice(five)
 print random.choice(seven)
