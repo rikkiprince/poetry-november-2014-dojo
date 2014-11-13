edc102bb14275a597f7b38bd13eceaca00c47603
Matt R Taylor <matt.ross.taylor@gmail.com>
2014-11-13 20:47:36 +0000

    adding additional images

--- poem.py
+++ poem.py
@@ -1,12 +1,17 @@
 #!/bin/usr/python
 
 from flask import Flask, render_template
+from five import five
+from seven import seven
+import random
+
+images = [ "static/1.jpg","static/2.jpg","static/3.jpg","static/4.jpg", "static/5.jpg" ]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html', image="1.jpg")
+	return render_template('home.html', image=random.choice(images), line1=random.choice(five),line2=random.choice(seven),line3=random.choice(five))
 
 if __name__ == "__main__":
 	app.debug = True
